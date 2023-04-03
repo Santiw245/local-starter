0a9682c51363ee4da3db69fc2146bc18021e3e03
Santiw245 <santiw245@gmail.com>
2023-04-03 08:05:34 +0700

    total price

--- app/src/main/java/com/example/localetextstarter/MainActivity.java
+++ app/src/main/java/com/example/localetextstarter/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.localetextstarter;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -16,10 +23,17 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import org.w3c.dom.Text;
 
 import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.Date;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText inputPrice;
+    TextView resultPrice, country;
+    Button countPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +56,14 @@ public class MainActivity extends AppCompatActivity {
         String formatDate = DateFormat.getDateInstance().format(myDate);
         TextView expiredTextView = findViewById(R.id.date);
         expiredTextView.setText(formatDate);
+
+        inputPrice = findViewById(R.id.inputHarga);
+        resultPrice = findViewById(R.id.textTotalHarga);
+        countPrice = findViewById(R.id.btnTotal);
+        country = findViewById(R.id.country);
+
+        //call method
+        convertPrice();
 
     }
 
@@ -90,5 +112,36 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void convertPrice(){
+
+        //get Resource Locale
+        Locale currentLocale = getResources().getConfiguration().locale;
+
+        //get country
+        TextView country = findViewById(R.id.country);
+        country.setText(currentLocale.getCountry());
+        String lang = country.getText().toString();
+
+        //Conditions when the button is clicked
+        countPrice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Double priceItem = Double.parseDouble(inputPrice.getText().toString());
+                Double price100Item = priceItem * 100;
+                //Convert Currency
+                if (lang.equals("US")){
+                    Double price = price100Item / 14968.65;
+                    resultPrice.setText(NumberFormat.getCurrencyInstance(currentLocale).format(price));
+                } else if (lang.equals("SA")) {
+                    Double price = price100Item / 3989.17;
+                    resultPrice.setText(NumberFormat.getCurrencyInstance(currentLocale).format(price));
+                }else {
+                    resultPrice.setText(NumberFormat.getCurrencyInstance(currentLocale).format(price100Item));
+                }
+            }
+        });
+
     }
 }
